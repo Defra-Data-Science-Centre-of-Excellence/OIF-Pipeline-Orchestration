cd002a0a099cae7f4535a5511e4954f01e1c5fe7
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-02-16 09:38:11 +0000

    initial commit

---
+++ install_jupyterhub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo python3 -m venv /opt/jupyterhub/
+
+sudo /opt/jupyterhub/bin/python3 -m pip install wheel
+sudo /opt/jupyterhub/bin/python3 -m pip install jupyterhub jupyterlab
+sudo /opt/jupyterhub/bin/python3 -m pip install ipywidgets
