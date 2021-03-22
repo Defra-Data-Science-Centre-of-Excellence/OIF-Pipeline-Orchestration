54a8dda177a59e673bde8c52f19c679d027442f8
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-22 14:28:17 +0000

    initial commit

---
+++ installation_scripts/install_rbenv_ruby_bundler.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+cd $HOME
+
+sudo apt-get remove ruby
+sudo apt-get update 
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+
+rbenv install 2.7.2
+rbenv local 2.7.2
+
+gem install bundler
+gem install jekyll
