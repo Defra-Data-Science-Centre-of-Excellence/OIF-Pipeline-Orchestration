2d054a6f91f032da058245fd0309eb2b76ee0dd6
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-22 14:27:45 +0000

    add user-specific git and logging

--- check_pull_copy.sh
+++ check_pull_copy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd /home/oif/OIF-Dashboard-Site \
-&& git fetch origin \
-&& git merge -s recursive -X theirs origin \
-&& bundle exec jekyll build -d /var/www/OIF-Dashboard-Site \
+&& /usr/bin/git fetch origin \
+&& if /usr/bin/git diff-index --quiet HEAD -- 
+    then
+    logger "merging changes from remote" \ 
+    && /usr/bin/git merge -s recursive -X theirs origin \
+    logger "rebuilding site" \
+    && bundle exec jekyll build -d /var/www/OIF-Dashboard-Site
+    else
+    logger "site already up-to-date"
+fi \
 && cd ~
