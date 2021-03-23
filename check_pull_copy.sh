0b36427b84f55608ca3b272ccc203fe1c7171cae
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-23 08:34:22 +0000

    log after execution

---
+++ check_pull_copy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/oif/OIF-Dashboard-Site;
+
+git fetch;
+
+LOCAL=$(git rev-parse @);
+REMOTE=$(git rev-parse @{u});
+
+if [[ $LOCAL != $REMOTE ]]; then
+    git reset --hard @{u} \
+    && logger "reset local repo to match remote";
+    bundle exec jekyll build -d /var/www/OIF-Dashboard-Site \
+    && logger "rebuilt site";
+else
+    logger "site already up-to-date";
+fi
+
+cd ~;
