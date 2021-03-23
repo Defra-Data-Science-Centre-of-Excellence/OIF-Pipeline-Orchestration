3de29a79dfdcc72f7897a8e73fdb0b3370175579
Ed Fawcett-Taylor <ed.fawcett-taylor@defra.gov.uk>
2021-03-23 13:39:51 +0000

    specify which bundle

--- check_pull_copy.sh
+++ check_pull_copy.sh
@@ -9,7 +9,9 @@ REMOTE=$(git rev-parse @{u});
 
 if [[ $LOCAL != $REMOTE ]]; then
     git reset --hard @{u} \
-    && logger "reset local repo to match remote";
+    && logger "reset local repo to match remote" \
+    && /home/oif/.rbenv/shims/bundle exec jekyll build -d /var/www/OIF-Dashboard-Site/ \
+    && logger "rebuilt site";
 else
     logger "site already up-to-date";
 fi
